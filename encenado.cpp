c56a4992e089c89d542a2e93c480f8d91d5a4c9b
Kauan <kauanavelinoa06@gmail.com>
2026-03-14 17:23:16 -0300

    Implement salary calculation and file I/O
    Read input from 'entrada' file, calculate net value, and write output to 'saida' file.

---
+++ encenado.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+int main(){
+	
+	int diasTra;
+	float valorLiqu, bruto;
+	ifstream entrada("entrada");
+	
+	entrada >> diasTra;
+	
+	bruto = diasTra * 60;
+	
+	valorLiqu = bruto - (bruto * 0.08);
+	
+	
+	
+	
+	ofstream saida("saida");
+	
+	saida << "R$ " << valorLiqu<< endl;
+	
+	saida.close();
+	return 0;
+}
