6886354d6ec33165d4872261bb331550070146dd
Kauan <kauanavelinoa06@gmail.com>
2026-03-14 21:31:37 -0300

    Update salario.cpp

--- Exercicio-01/salario.cpp
+++ Exercicio-01/salario.cpp
@@ -1,1 +1,18 @@
-
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()  {
+	ofstream arquivo ("salario.txt");
+	float real, dolar, euro, libra;
+	cin>>real;
+	
+	dolar = real / 2.13;
+	euro = real / 2.84;
+	libra = real / 3.34;
+	
+	arquivo <<dolar<<endl;
+	arquivo <<euro<<endl;
+	arquivo << libra<<endl;
+	arquivo.close();
+	return 0;
+}
