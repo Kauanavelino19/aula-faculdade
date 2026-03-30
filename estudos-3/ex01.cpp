3f915d2250ef5313ac244e4970247d11e7547c96
Kauan <kauanavelinoa06@gmail.com>
2026-03-30 11:13:15 -0300

    Add files via upload

---
+++ estudos-3/ex01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+int main (){
+	string ler, nomeArquivo;
+	cin >> nomeArquivo;
+	ifstream entrada(nomeArquivo);
+
+	entrada >> ler;
+	cout << ler << endl;
+	
+	entrada.close();
+	
+return 0;	
+}
