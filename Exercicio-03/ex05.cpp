040c896b4164b5857f7dc2b0c993718ad361f67c
Kauan <kauanavelinoa06@gmail.com>
2026-03-31 13:55:26 -0300

    Add weighted average calculation in ex05.cpp
    Implement weighted average calculation from file input.

---
+++ Exercicio-03/ex05.cpp
@@ -0,0 +1,31 @@
+//Média Ponderada (arquivo)
+/*
+Esse eu consegui resolver sem IA, me senti orgurlhoso com isso
+*/
+
+#include <iostream> // biblioteca de fluxo da entrada e saida(cout, cin) 
+#include <fstream> // biblioteca para arquivo 
+#include <iomanip> // biblioteca para utilizar o fixed e setprecision()
+
+using namespace std; //permitindo a usar o elemento sa bibliotea padrão (cout, cin)
+int main (){
+	int nota1, peso1, nota2, peso2, nota3, peso3; // variável inteiro
+	float soma1, soma2, mediaP; //variável ponto flutuante
+	ifstream entrada; // variável de arquivo de entrada (ifstream)
+	entrada.open("notas.txt"); //criando um arquivo de entrada
+	entrada >> nota1 >> peso1; //fazendo a entrada de dados em arquivo
+	entrada >> nota2 >> peso2;//fazendo a entrada de dados em arquivo
+	entrada >> nota3 >> peso3;//fazendo a entrada de dados em arquivo
+	ofstream saida; // variável de arquivo de saida (ofstream)
+	saida.open("final.txt"); //criando um arquivo de sauda
+	
+	 //somas
+	soma1 = (nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3); 
+	soma2 = peso1 + peso2 + peso3;
+	mediaP = soma1 / soma2;
+
+	saida << fixed << setprecision(2)<< mediaP << endl;
+	entrada.close();
+	saida.close();
+    return 0;
+}
